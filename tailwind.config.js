240d19c9413a5c5a73939eb113866b4a7728f51b
GeorgeNonis <georgenonis@gmail.com>
2023-10-31 13:34:47 +0100

    done with setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      "2xl": "1536px",
+    },
+  },
+  variants: {},
+  plugins: [],
+};
